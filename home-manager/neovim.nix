e63b23df6856ddedf37f34cc7691c3892817a45a
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-01 03:23:47 +0000

    Load nvim folder

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -5,9 +5,9 @@
     vimAlias = true;
 
     # ~/.config/nvim/init.luaに文字列が展開される
-    extraLuaConfig = builtins.readFile ./init.lua;
+    # extraLuaConfig = builtins.readFile ./init.lua;
   };
-  # xdg.configFile."nvim" = {
-  #   source = ./nvim;
-  # };
+  xdg.configFile."nvim" = {
+    source = ../nvim;
+  };
 }
