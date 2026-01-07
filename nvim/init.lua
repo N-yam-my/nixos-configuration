0f7f565b6129ad353f4dfee3d8191fa13a224089
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-07 03:54:40 +0000

    Refactor Neovim plugin management

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,24 +6,40 @@ require('settings.autocmd')
 require('settings.keymaps')
 
 -- bootstrap
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
 
 -- load plugins with lazy.nvim
 local opts = {
   defaults = {
     lazy = true,
-  }
+  },
+  spec = nil, -- WIP
+  rocks = {
+    enabled = false,
+  },
+  dev = {
+    path = "~/projects", -- WIP
+  },
+  performance = {
+    reset_packpath = false,
+    rtp = {
+      reset = falsem
+    },
+  },
+  install = {
+    missing = false,
+  },
 }
 require("lazy").setup("plugins", opts)
 
--- nvim/lua/settings/autocmd.lua
+++ nvim/lua/settings/autocmd.lua
@@ -11,12 +11,3 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.api.nvim_create_autocmd(
---     {
---       "BufWritePost"
---     },
---     {
---         pattern = "*.typ",
---         command = "!typst compile %"
---     }
--- )
