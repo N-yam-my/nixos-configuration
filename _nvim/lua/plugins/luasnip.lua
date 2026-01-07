5ec5da4a92a6d67c0fd7644f11c15e0bd36b1e1e
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-07 03:55:00 +0000

    Refactor Neovim plugin management

---
+++ _nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    'L3MON4D3/LuaSnip',
+    -- follow latest release.
+    version = 'v2.*', --Replace <CurrentMajor> by the latest release major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = 'make install_jsregexp',
+    event = 'InsertEnter',
+    config = function()
+      -- local ls = require('luasnip')
+      -- vim.keymap.set({'i'}, '<Tab>', function()
+      --   if ls.expand_or_jumpable() == true then
+      --     ls.expand_or_jump()
+      --     return ''
+      --   else
+      --     local key = vim.api.nvim_replace_termcodes('<Tab>', true, false, true)
+      --     return vim.api.nvim_feedkeys(key, 'n', false)
+      --   end
+      -- end, { expr = false, silent = true })
+      -- vim.keymap.set({'i', 's'}, '<S-Tab>', function()
+      --   if ls.jumpable(-1) == true then
+      --     ls.jump(-1)
+      --     return ''
+      --   else
+      --     local key = vim.api.nvim_replace_termcodes('<S-Tab>', true, false, true)
+      --     return vim.api.nvim_feedkeys(key, 'n', false)
+      --   end
+      -- end, {expr = false, silent = true})
+      -- vim.keymap.set({'i', 's'}, '<C-E>', function()
+      --   if ls.choice_active() == true then
+      --     ls.change_choice(1)
+      --     return ''
+      --   end
+      -- end, { expr = false, silent = true })
+    end,
+  },
+}
