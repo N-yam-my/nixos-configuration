c081f04fae8bbe74adcaa3cb044d058b67793f42
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-08 04:16:29 +0000

    Fix typo

--- home-manager/neovim/base.nix
+++ home-manager/neovim/base.nix
@@ -18,12 +18,12 @@
 		];
 		# Install lazy.nvim
 		plugins = with pkgs.vimPlugins; [ lazy-nvim ];
-		#Expand into ~/.config/nvim/init.lua
-		# extraLuaConfig = builtins.readFile ./init.lua;
-		#Expand into ~/.config/nvim/lua
-		xdg.configFile."nvim/lua" = {
-			recursive = true;
-			source = ./lua;
-		};
+	};
+	#Expand into ~/.config/nvim/init.lua
+	# extraLuaConfig = builtins.readFile ./init.lua;
+	#Expand into ~/.config/nvim/lua
+	xdg.configFile."nvim/lua" = {
+		recursive = true;
+		source = ./lua;
 	};
 }
