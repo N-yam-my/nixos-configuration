8836290b311760023d2927c808dfc28a94d0febe
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-08 09:38:15 +0000

    Add path of TS parsers settings?

--- home-manager/neovim/treesitter-parser.nix
+++ home-manager/neovim/treesitter-parser.nix
@@ -1,24 +1,24 @@
 { pkgs, ... }:
-let
-  substituteStrings = import ../../lib/substituteStrings.nix;
-  parsers = plugins: with plugins; [
-    html
-    lua
-    nix
-    python
-    query
-    rust
-    toml
-    typst
-    vim
-    vimdoc
-  ];
-  parserPaths = (pkgs.vimPlugins.nvim-treesitter.withPlugins parsers).dependencies;
-in
+# let
+#   substituteStrings = import ../../lib/substituteStrings.nix;
+# in
 {
   # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
   xdg.dataFile."nvim/site/parser".source =
     let
+		  parsers = plugins: with plugins; [
+		    html
+		    lua
+		    nix
+		    python
+		    query
+		    rust
+		    toml
+		    typst
+		    vim
+		    vimdoc
+		  ];
+		  parserPaths = (pkgs.vimPlugins.nvim-treesitter.withPlugins parsers).dependencies;
       parserPath = pkgs.symlinkJoin {
         name = "treesitter-parsers";
         paths = parserPaths;
