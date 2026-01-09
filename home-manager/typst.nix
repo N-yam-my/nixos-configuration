bfa70477d62f932662fa80f03db6945c49f14914
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-10 00:31:21 +0900

    Set symlink for each typst local package

--- home-manager/typst.nix
+++ home-manager/typst.nix
@@ -1,8 +1,12 @@
 { pkgs, config, ... }:
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
-  misc = "${config.home.homeDirectory}/.dotfiles/misc";
+  local = "${config.home.homeDirectory}/.dotfiles/misc/typst/package/local";
 in
 {
-  xdg.dataFile."typst".source = mkOutOfStoreSymlink "${misc}/typst";
+  xdg.dataFile = {
+    "typst/package/local/myStyle".source = mkOutOfStoreSymlink "${local}/myStyle";
+    "typst/package/local/myPackages".source = mkOutOfStoreSymlink "${local}/myPackages";
+    "typst/package/local/myUtils".source = mkOutOfStoreSymlink "${local}/myUtils";
+  };
 }
