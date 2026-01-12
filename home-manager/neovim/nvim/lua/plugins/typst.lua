99b99f91cf8405710b8652213518be609c691d77
N-yam-my <hikonao.yama.1219@gmail.com>
2026-01-12 15:36:54 +0900

    No invert

--- home-manager/neovim/nvim/lua/plugins/typst.lua
+++ home-manager/neovim/nvim/lua/plugins/typst.lua
@@ -1,16 +1,14 @@
 return {
   {
     'chomosuke/typst-preview.nvim',
-    -- ft = 'typst',
     cmd = { 'TypstPreview', 'TypstPreviewToggle' },
     config = function()
-      -- local path = vim.fn.getenv("XDG_DATA_HOME")
       require 'typst-preview'.setup {
         -- debug = true,
         open_cmd = "msedge.exe %s",
         -- port = 8000,
         -- extra_args = {"--data-plane-host=127.0.0.1:8000", },
-        invert_colors = '{ "rest": "always", "image": "never" }',
+        -- invert_colors = '{ "rest": "always", "image": "never" }',
         -- follow_cursor = false,
         dependencies_bin = {
           ['tinymist'] = "tinymist",
