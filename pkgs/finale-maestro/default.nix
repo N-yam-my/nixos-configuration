31557d7a0809bb599ded595e259846b9bc6c6ddb
N-yam-my <hikonao.yama.1219@gmail.com>
2026-03-26 08:49:10 +0900

    Add finale maestro font

---
+++ pkgs/finale-maestro/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenvNoCC,
+  nix-update-script,
+  sources,
+  ...
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+
+  inherit (sources.finale-maestro) pname version src;
+  # src: fetchFromGithub
+  
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm444 *.otf -t $out/share/fonts/opentype/
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
+    description = "MakeMusic's Finale Maestro font";
+    homepage = "https://github.com/eNote-GmbH/Maestro";
+    license = lib.licenses.ofl;
+    # maintainers = with lib.maintainers; [
+    #   n-yam-my
+    # ];
+    platforms = lib.platforms.all;
+  };
+})
